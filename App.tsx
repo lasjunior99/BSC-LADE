69c4e7bf12d0345d4546061273a2e2ee6c546623
Luiz Alberto <lasjunior99@gmail.com>
2026-01-18 02:28:43 -0300

    Corrige App.tsx removendo duplicações e erro de export

--- App.tsx
+++ App.tsx
@@ -1,42 +1,38 @@
-
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import IdentidadeOrganizacional from './pages/IdentidadeOrganizacional';
-import ContratoResultados from './pages/ContratoResultados';
-import ProjetosEstrategicos from './pages/ProjetosEstrategicos';
-import Admin from './pages/Admin';
 import { useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Sidebar from "./components/Sidebar";
 
-function App() {
+import IdentidadeOrganizacional from "./pages/IdentidadeOrganizacional";
+import ContratoResultados from "./pages/ContratoResultados";
+import ProjetosEstrategicos from "./pages/ProjetosEstrategicos";
+import Admin from "./pages/Admin";
+
+const App: React.FC = () => {
 
   useEffect(() => {
     alert("REACT INICIALIZADO");
   }, []);
 
   return (
-    <div>
-      <Sidebar />
-      {/* resto do app */}
-    </div>
-  );
-}
-
-export default App;
-
-const App: React.FC = () => {
-  return (
     <Router>
       <div className="flex h-screen overflow-hidden bg-background-light dark:bg-background-dark">
         <Sidebar />
+
         <main className="flex-1 flex flex-col min-w-0 overflow-y-auto">
           <Routes>
             <Route path="/" element={<IdentidadeOrganizacional />} />
             <Route path="/contrato-resultados" element={<ContratoResultados />} />
             <Route path="/projetos" element={<ProjetosEstrategicos />} />
             <Route path="/admin" element={<Admin />} />
-            <Route path="/dashboards" element={<div className="p-10 text-center">Página em Desenvolvimento</div>} />
+            <Route
+              path="/dashboards"
+              element={
+                <div className="p-10 text-center">
+                  Página em Desenvolvimento
+                </div>
+              }
+            />
           </Routes>
         </main>
       </div>
@@ -45,3 +41,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
